aeeb440b5fee6806a701d913793aa3d959396304
CodeNinjasSJ <57017893+CodeNinjasSJ@users.noreply.github.com>
2023-03-02 18:55:31 -0700

    sum
    rum

---
+++ Assets/scr.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotationToAdd = new Vector3(0, .1f, 0);
+        transform.Rotate(rotationToAdd);
+    }
+}
